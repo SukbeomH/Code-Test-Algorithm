4800626e034a0486acf6e47bd7ebc9569c954dfc
SukbeomH <brent93.dev@gmail.com>
2023-12-21 11:22:18 +0900

    BOJ 1009

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // require('fs').readFileSync('/dev/stdin')
 // require('fs').readFileSync('testCase.txt')
-const input = require('fs').readFileSync('testCase.txt').toString();
+const input = require('fs').readFileSync('testCase.txt').toString().split('\n')
+
+const line = input;
+const t = Number(line[0]);
+
+let number = 0n;
+let answer = '';
+for (let i = 1; i <= t; i++) {
+  const [a, b] = line[i].split(' ');
+  number = BigInt(a) ** BigInt(b);
+  answer += number % 10n + '\n';
+}
+
+console.log(answer);
