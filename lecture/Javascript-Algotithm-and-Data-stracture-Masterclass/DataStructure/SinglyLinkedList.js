b1210fe1b3f2bdacd221f3bc6ad3eb1186b26e2e
SukbeomH <brent93.dev@gmail.com>
2024-01-04 11:23:03 +0900

    Singly Linked List, push

--- lecture/Javascript-Algotithm-and-Data-stracture-Masterclass/DataStructure/SinglyLinkedList.js
+++ lecture/Javascript-Algotithm-and-Data-stracture-Masterclass/DataStructure/SinglyLinkedList.js
@@ -1,6 +1,33 @@
 class node{
-  constructor(val, next) {
+  constructor(val) {
     this.val = val;
-    this.next = next;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    // 새로운 노드 생성
+    let newNode = new node(val);
+    // 리스트가 비어있을 때
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    // 리스트가 비어있지 않을 때
+    else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    // 길이 증가
+    this.length++;
+    
+    return this;
   }
 }
