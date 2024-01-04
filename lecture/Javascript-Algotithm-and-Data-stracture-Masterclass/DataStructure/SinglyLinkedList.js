1c80ea397b2c69214aae95a543497350a35a5f50
SukbeomH <brent93.dev@gmail.com>
2024-01-04 11:33:51 +0900

    SLL-pop

--- lecture/Javascript-Algotithm-and-Data-stracture-Masterclass/DataStructure/SinglyLinkedList.js
+++ lecture/Javascript-Algotithm-and-Data-stracture-Masterclass/DataStructure/SinglyLinkedList.js
@@ -27,7 +27,31 @@ class SinglyLinkedList {
     }
     // 길이 증가
     this.length++;
-    
+
     return this;
+  }
+
+  pop() {
+    // 리스트가 비어있을 때
+    if (!this.head) return undefined;
+    // 리스트가 비어있지 않을 때
+    else {
+      let current = this.head;
+      let newTail = current;
+      while (current.next) {
+        newTail = current;
+        current = current.next;
+      }
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+      // 리스트가 비어있을 때
+      if (this.length === 0) {
+        this.head = null;
+        this.tail = null;
+      }
+
+      return current;
+    }
   }
 }
