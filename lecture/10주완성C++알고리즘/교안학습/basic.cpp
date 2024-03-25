4ad3169d6a91d6744d8897b09d428de6e0c19674
SukbeomH <brent93.dev@gmail.com>
2024-03-25 15:30:14 +0900

    update learn

---
+++ lecture/10주완성C++알고리즘/교안학습/basic.cpp
@@ -0,0 +1,98 @@
+// #include <bits/stdc++.h> // 헤더파일 선언
+// using namespace std; // std namespace 사용
+
+// string a; // 문자열 변수 a 선언
+
+// int main(){
+//   cin >> a; // 입력받은 문자열을 a에 저장
+//   cout << a << "\n"; // 저장된 문자열을 출력
+
+//   return 0; // main 함수 종료
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// typedef int i; // int 타입을 별칭 i 로 선언
+
+// i main(){
+//   i a = 1;
+//   cout << a << "\n";
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define PI 3.141592
+// #define loop(x, n) for (int x = 0; x < n; x++)
+
+// int main(){
+//   cout << PI << '\n';
+//   int sum = 0;
+
+//   loop(i, 10){
+//     sum += i;
+//   }
+
+//   cout << sum << '\n';
+
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int a, c;
+// string b;
+
+// int main(){
+//   cin >> a >> b >> c;
+//   scanf("%d", &a);
+
+//   cout << a << '\n';
+//   cout << b << '\n';
+//   cout << c << '\n';
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int a;
+// double b;
+// char c;
+
+// int main() {
+//   scanf("%d %lf %c", &a, &b, &c);
+//   printf("%d\n", a);
+//   printf("%lf\n", b);
+//   printf("%c\n", c);
+
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int a, b;
+// double c;
+
+// int main(){
+//   scanf("%d.%d", &a, &b);
+//   printf("\n%d %d\n", a, b);
+
+//   return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+int main(){
+  getline(cin, s, '\n');
+  cout << s << '\n';
+
+  return 0;
+}
