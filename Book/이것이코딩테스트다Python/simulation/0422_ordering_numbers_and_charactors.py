6f2a1a6f01b3c86f095f98432e75a5f52d2d5ad5
SukbeomH <brent93.dev@gmail.com>
2024-04-22 14:20:56 +0900

    add

--- Book/이것이코딩테스트다Python/simulation/0422_ordering_numbers_and_charactors.py
+++ Book/이것이코딩테스트다Python/simulation/0422_ordering_numbers_and_charactors.py
@@ -1,1 +1,9 @@
-def main():
+def main(str):
+    char = ""
+    num = 0
+    for s in str:
+        if type(int(s)) is not type(0):
+            char = char + s
+        else:
+            num += int(s)
+    return char + num
